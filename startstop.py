5796549cf340d3c4e0cb77ff6150a9f79089f6cf
rahulhm <113351057+rahulhm@users.noreply.github.com>
2023-04-18 18:44:57 +0530

    first commit

---
+++ startstop.py
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Set your resource group name and VM name
+resource_group_name="mc-dev-env"
+vm_name="catalog-service"
+
+# Start the VM using az command
+az vm start --resource-group $resource_group_name --name $vm_name
+
+# Stop the VM using az command
+az vm deallocate --resource-group $resource_group_name --name $vm_name
